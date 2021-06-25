39f0d9e80b3d4f3a8a0005644dc46c5c42cb5d9e
Maksym Hanushchak <hanushchak@outlook.com>
2021-06-25 17:47:12 -0400

    Added Upload Controller

---
+++ RailGallery/Controllers/UploadController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RailGallery.Data;
+using RailGallery.Models;
+
+namespace RailGallery.Controllers
+{
+    //[Authorize]
+    public class UploadController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UploadController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Upload
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        // GET: Upload/History
+        public async Task<IActionResult> History()
+        {
+            return View(await _context.Images.ToListAsync());
+        }
+
+        // GET: Upload/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var image = await _context.Images
+                .FirstOrDefaultAsync(m => m.ImageID == id);
+            if (image == null)
+            {
+                return NotFound();
+            }
+
+            return View(image);
+        }
+
+        // POST: Upload/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ImageID,ImageTitle,ImageDescription,ImageMetadata,ImageTakenDate,ImageUploadedDate,ImageStatus,ImagePrivacy")] Image image)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(image);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(image);
+        }
+
+        // GET: Upload/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var image = await _context.Images.FindAsync(id);
+            if (image == null)
+            {
+                return NotFound();
+            }
+            return View(image);
+        }
+
+        // POST: Upload/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ImageID,ImageTitle,ImageDescription,ImageMetadata,ImageTakenDate,ImageUploadedDate,ImageStatus,ImagePrivacy")] Image image)
+        {
+            if (id != image.ImageID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(image);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ImageExists(image.ImageID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(image);
+        }
+
+        // GET: Upload/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var image = await _context.Images
+                .FirstOrDefaultAsync(m => m.ImageID == id);
+            if (image == null)
+            {
+                return NotFound();
+            }
+
+            return View(image);
+        }
+
+        // POST: Upload/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var image = await _context.Images.FindAsync(id);
+            _context.Images.Remove(image);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ImageExists(int id)
+        {
+            return _context.Images.Any(e => e.ImageID == id);
+        }
+    }
+}
--- RailGallery/Models/Image.cs
+++ RailGallery/Models/Image.cs
@@ -1,27 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace RailGallery.Models
 {
     public enum Status
     {
-        Pending, Published, Rejected
+        Pending, 
+        Published, 
+        Rejected
     }
 
     public enum Privacy
     {
-        Public, Private
+        Public, 
+        Private
     }
 
     public class Image
     {
+        [Display(Name = "ID")]
         public int ImageID { get; set; }
+        [Required]
+        [Display(Name = "Title")]
         public string ImageTitle { get; set; }
+        [Display(Name = "Description")]
         public string ImageDescription { get; set; }
+        [Display(Name = "EXIF")]
         public string ImageMetadata { get; set; }
+        [Display(Name = "Date Taken")]
         public DateTime ImageTakenDate { get; set; }
+        [Display(Name = "Date Uploaded")]
         public DateTime ImageUploadedDate { get; set; }
+        [Display(Name = "Status")]
         public Status ImageStatus { get; set; }
+        [Display(Name = "Privacy")]
         public Privacy ImagePrivacy { get; set; }
 
         public ICollection<Comment> Comments { get; set; }
