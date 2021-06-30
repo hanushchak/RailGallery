0138d5e4700256e170715d7ba56bb42d736bc508
Maksym Hanushchak <hanushchak@outlook.com>
2021-06-30 19:37:19 -0400

    Added View Controller

---
+++ RailGallery/Controllers/ViewController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RailGallery.Data;
+using RailGallery.Models;
+
+namespace RailGallery.Controllers
+{
+    public class ViewController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ViewController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: View
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Images.ToListAsync());
+        }
+
+        // GET: View/5
+        [Route("View/{id}")]
+        public async Task<IActionResult> View(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var image = await _context.Images
+                .Include(m => m.Comments)
+                .Include(m => m.Likes)
+                .Include(m => m.Albums)
+                .Include(m => m.Favorites)
+                .Include(m => m.Category)
+                .Include(m => m.ApplicationUser)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.ImageID == id);
+            
+            if (image == null)
+            {
+                return NotFound();
+            }
+
+            return View(image);
+        }
+
+        /*// GET: View/Create
+        public IActionResult Create()
+        {
+            return View();
+        }*/
+
+        // POST: View/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        /*[HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ImageID,ImageTitle,ImageDescription,ImageMetadata,ImageTakenDate,ImageUploadedDate,ImageStatus,ImagePrivacy,ImagePath")] Image image)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(image);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(image);
+        }*/
+
+        // GET: View/Edit/5
+        /*public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var image = await _context.Images.FindAsync(id);
+            if (image == null)
+            {
+                return NotFound();
+            }
+            return View(image);
+        }*/
+
+        // POST: View/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        /*[HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ImageID,ImageTitle,ImageDescription,ImageMetadata,ImageTakenDate,ImageUploadedDate,ImageStatus,ImagePrivacy,ImagePath")] Image image)
+        {
+            if (id != image.ImageID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(image);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ImageExists(image.ImageID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(image);
+        }*/
+
+        // GET: View/Delete/5
+        /*public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var image = await _context.Images
+                .FirstOrDefaultAsync(m => m.ImageID == id);
+            if (image == null)
+            {
+                return NotFound();
+            }
+
+            return View(image);
+        }*/
+
+        // POST: View/Delete/5
+        /*[HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var image = await _context.Images.FindAsync(id);
+            _context.Images.Remove(image);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }*/
+
+        private bool ImageExists(int id)
+        {
+            return _context.Images.Any(e => e.ImageID == id);
+        }
+    }
+}
