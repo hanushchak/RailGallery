29db404ff5b9ddfc0931383dea8f7db9abdfad4e
Maksym Hanushchak <hanushchak@outlook.com>
2021-09-10 11:54:47 -0400

    Seeded User Roles

---
+++ RailGallery/Data/ContextSeed.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using RailGallery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RailGallery.Data
+{
+    public class ContextSeed
+    {
+        public static async Task SeedRolesAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            // Seed Roles
+            await roleManager.CreateAsync(new IdentityRole(Enums.Roles.Moderator.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Enums.Roles.BasicUser.ToString()));
+        }
+    }
+}
