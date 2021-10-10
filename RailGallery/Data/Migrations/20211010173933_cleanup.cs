889e869e0b925da22c075809afa212c43c6b0c26
Maksym Hanushchak <hanushchak@outlook.com>
2021-10-10 13:41:35 -0400

    Added migration after code cleanup

---
+++ RailGallery/Data/Migrations/20211010173933_cleanup.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RailGallery.Data.Migrations
+{
+    public partial class cleanup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Images_Categories_CategoryID",
+                table: "Images");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Images_Locations_LocationID",
+                table: "Images");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LocationID",
+                table: "Images",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryID",
+                table: "Images",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Images_Categories_CategoryID",
+                table: "Images",
+                column: "CategoryID",
+                principalTable: "Categories",
+                principalColumn: "CategoryID",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Images_Locations_LocationID",
+                table: "Images",
+                column: "LocationID",
+                principalTable: "Locations",
+                principalColumn: "LocationID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Images_Categories_CategoryID",
+                table: "Images");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Images_Locations_LocationID",
+                table: "Images");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LocationID",
+                table: "Images",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryID",
+                table: "Images",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Images_Categories_CategoryID",
+                table: "Images",
+                column: "CategoryID",
+                principalTable: "Categories",
+                principalColumn: "CategoryID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Images_Locations_LocationID",
+                table: "Images",
+                column: "LocationID",
+                principalTable: "Locations",
+                principalColumn: "LocationID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
